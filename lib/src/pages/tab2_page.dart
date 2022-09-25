0b832239575b5ad081ca1feb8a3f8e904ebab9e5
Nacho <44799327+nachom96@users.noreply.github.com>
2022-09-25 11:02:00 +0200

    Lista de categorías añadida

--- lib/src/pages/tab2_page.dart
+++ lib/src/pages/tab2_page.dart
@@ -1,13 +1,46 @@
 import 'package:flutter/material.dart';
+import 'package:newsapp/src/services/news_service.dart';
+import 'package:provider/provider.dart';
 
 class Tab2Page extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Center(
-        child: Text('Luthadel'),
+    return SafeArea(
+      child: Scaffold(
+        body: Column(
+          children: [
+            Expanded(child: _ListaCategorias()),
+          ],
+        )
       ),
+    );
+  }
+}
+
+class _ListaCategorias extends StatelessWidget {
+
+  @override
+  Widget build(BuildContext context) {
+
+    final categories = Provider.of<NewsService>(context).categories;
+
+    return ListView.builder(
+      physics: const BouncingScrollPhysics(),
+      scrollDirection: Axis.horizontal,
+      itemCount: categories.length,
+      itemBuilder: (BuildContext context, int index) {
+        return Padding(
+          padding: EdgeInsets.all(8),
+          child: Column(
+            children: [
+              Icon( categories[index].icon),
+              const SizedBox(height: 5,),
+              Text( categories[index].name ),
+            ],
+          ),
+          );
+      },
     );
   }
 }
