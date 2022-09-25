597f003ed67f45182c6b0ec42970a0d38f69f778
Nacho <44799327+nachom96@users.noreply.github.com>
2022-09-25 11:14:32 +0200

    Menú categorías mejorado

--- lib/src/pages/tab2_page.dart
+++ lib/src/pages/tab2_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:newsapp/src/models/category_model.dart';
 import 'package:newsapp/src/services/news_service.dart';
 import 'package:provider/provider.dart';
 
@@ -30,17 +31,52 @@ class _ListaCategorias extends StatelessWidget {
       scrollDirection: Axis.horizontal,
       itemCount: categories.length,
       itemBuilder: (BuildContext context, int index) {
-        return Padding(
-          padding: EdgeInsets.all(8),
-          child: Column(
-            children: [
-              Icon( categories[index].icon),
-              const SizedBox(height: 5,),
-              Text( categories[index].name ),
-            ],
-          ),
-          );
+
+        final cName = categories[index].name; // Para capitalizar el texto
+
+        // Este SizedBox es simplemente para que haya un poquito de separación
+        return SizedBox(
+          width: 110,
+          child: Padding(
+            padding: const EdgeInsets.all(8),
+            child: Column(
+              children: [
+                _CategoryButton(categoria: categories[index]),
+                const SizedBox(height: 5,),
+                Text( '${ cName[0].toUpperCase() }${cName.substring(1)}'),
+              ],
+            ),
+            ),
+        );
       },
+    );
+  }
+}
+
+class _CategoryButton extends StatelessWidget {
+
+  final Category categoria;
+
+  const _CategoryButton({required this.categoria});
+
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: (){ print('${categoria.name}');},
+      child: Container(
+        width: 40,
+        height: 40,
+        margin: const EdgeInsets.symmetric(horizontal: 10),
+        decoration: const BoxDecoration(
+          shape: BoxShape.circle,
+          color: Colors.white,        
+        ),
+        child: Icon(
+          categoria.icon,
+          color: Colors.black54,
+        ),
+      ),
     );
   }
 }
